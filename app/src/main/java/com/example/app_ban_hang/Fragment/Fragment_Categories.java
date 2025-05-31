160da66a9e6b4efadd2d5b7e1cd3a1830a0fec50
danh_thanh <tinhoc7649@gmail.com>
2025-05-31 09:10:44 +0700

    Giao diện Address +Cart + Categories + Detai; + Invoice + Product

---
+++ app/src/main/java/com/example/app_ban_hang/Fragment/Fragment_Categories.java
@@ -0,0 +1,50 @@
+package com.example.app_ban_hang.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.app_ban_hang.R;
+
+public class Fragment_Categories extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public Fragment_Categories() {
+        // Required empty public constructor
+    }
+
+
+    public static Fragment_Categories newInstance(String param1, String param2) {
+        Fragment_Categories fragment = new Fragment_Categories();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment__categories, container, false);
+    }
+}
