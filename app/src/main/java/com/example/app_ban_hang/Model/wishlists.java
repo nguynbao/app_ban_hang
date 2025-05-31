a0459c222b2326e7f9c14ca6616b4ecdcd3242e8
danh_thanh <tinhoc7649@gmail.com>
2025-05-31 13:44:43 +0700

    update model + database

---
+++ app/src/main/java/com/example/app_ban_hang/Model/wishlists.java
@@ -0,0 +1,31 @@
+package com.example.app_ban_hang.Model;
+
+public class wishlists {
+    private int wishlistId;
+    private int userId;
+    private int productId;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getWishlistId() {
+        return wishlistId;
+    }
+
+    public void setWishlistId(int wishlistId) {
+        this.wishlistId = wishlistId;
+    }
+}
